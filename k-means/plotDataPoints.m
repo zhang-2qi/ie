bf327bdd40b8a989ad157d2c41646c54cb214d9d
zhang-2qi <596353954@qq.com>
2017-03-05 17:42:40 +0800

    加了一大堆功能后的

--- k-means/plotDataPoints.m
+++ k-means/plotDataPoints.m
@@ -1,8 +1,8 @@
-function plotDataPoints(X, idx, K)
+function [handA] = plotDataPoints(X, idx, K)
  palette = hsv(K + 1);
  colors = palette(idx, :);
 
 % Plot the data
-scatter3(X(:,1), X(:,2),X(:,3),15, colors);
-
+handA = scatter3(X(:,1), X(:,2),X(:,3),15, colors);
+%saveas(handA);
 end
